6f1256b72c40048068f40bfe5ef0682816f9ba58
Angela Vawser <amvawser@gmail.com>
2019-07-12 09:49:01 -0700

    pic changes, content edit

---
+++ app.js
@@ -0,0 +1,11 @@
+import isEven from './is-even.js';
+import isOdd from './is-odd.js';
+
+// reference elements
+const isEven = document.getElementById('even');
+const isOdd = document.getElementById('odd');
+
+// wire up events
+ function getResult() {
+     numberInput.textContent = evenClown.value;
+ }
