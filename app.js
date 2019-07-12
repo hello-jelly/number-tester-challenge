d84ecc827763c838568eb0b6ded2840188556360
Angela Vawser <amvawser@gmail.com>
2019-07-12 12:28:11 -0700

    still figuring out code

--- app.js
+++ app.js
@@ -2,12 +2,11 @@ import isEven from './is-even.js';
 import isOdd from './is-odd.js';
 
 // reference elements
-const isEven = document.getElementById('even');
-const isOdd = document.getElementById('odd');
+const evenClown = document.getElementById('even');
+const oddClown = document.getElementById('odd');
 const x = document.getElementById('numberInput');
 
 // wire up events
- function getResult() {
-    const x = Number(isEven.value);
-    const x = Number(isOdd.value)
-}
+button.onClick = getResult(x) {
+    x = Number(evenClown.value);
+    const x = Number(oddClown.value)
