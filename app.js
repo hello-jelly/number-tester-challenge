e55e6b976227c45cccba214d71109c5d261d80dc
Angela Vawser <amvawser@gmail.com>
2019-07-12 09:56:51 -0700

    small change to js

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ import isOdd from './is-odd.js';
 // reference elements
 const isEven = document.getElementById('even');
 const isOdd = document.getElementById('odd');
+const x = document.getElementById('numberInput');
 
 // wire up events
  function getResult() {
-     numberInput.textContent = evenClown.value;
+     numberInput.textContent = isEven.value;
  }
